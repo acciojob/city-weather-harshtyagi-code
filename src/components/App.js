3e8a36b0665ef2ba9548159fa9374f616e6a8182
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-03-27 05:01:59 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,55 @@
-
-import React, { useState } from "react";
-import axios from "axios";
-import 'regenerator-runtime/runtime'
-
-const API_KEY = "4054535fcc56ee7b217bf7f35be322f1";
-
-function App() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState(null);
-
-  const search = async (e) => {
-    if (e.key === "Enter") {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
-      );
-      setWeather(response.data);
-      setQuery("");
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
+ 
+const API_KEY = "ba2d767c7354ee6337b93936ec909c9a";
+ 
+const App = () => {
+  let [search, setSearch] = useState("");
+  let [data, setData] = useState("");
+ 
+  useEffect(() => {
+    let timer
+    if(timer){
+      clearTimeout(timer)
     }
-  };
-
-  const kelvinToFahrenheit = (k) => ((k - 273.15) * 9) / 5 + 32;
-
+    timer =setTimeout(() => {
+      console.log(search)
+      // Send Axios request here
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`)
+      .then(res => (res.json()))
+      .then(res => {
+        console.log(res);
+        console.log(res.weather[0]);
+        setData(res);
+        setSearch("")
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+    },1000)
+  }, [search])
+ 
   return (
-    <div className="app">
-      <input
-        type="text"
-        className="search"
-        placeholder="Enter a city"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
-      {weather && (
-        <div className="weather">
-          <div className="city">{weather.name}</div>
-          <div className="temperature">
-            {Math.round(kelvinToFahrenheit(weather.main.temp))}°F
+    <div>
+      {/* Do not remove the main div */}
+      <div className="input-div">
+        <input
+          type="search"
+          className="search"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        ></input>
+      </div>
+        {
+          data &&
+          <div className="weather">
+            <div>{data.name}</div>
+            <div>{data.main.temp}F</div>
+            <div>{data.weather[0].description}</div>
           </div>
-          <div className="description">{weather.weather[0].description}</div>
-          <div className="icon">
-            <img
-              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-              alt={weather.weather[0].description}
-            />
-          </div>
-        </div>
-      )}
+        }     
     </div>
   );
-}
-
+};
+ 
 export default App;
